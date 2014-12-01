718e428b28b2ab1ed8acc181f373abd42b59063d
adrienxp3 <158113@supinfo.com>
2014-12-01 14:16:19 +0100

    création classe toto pour tester

---
+++ src/conf/Toto.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author adrienxp3
+ */
+public class Toto {
+    public String name;
+    
+}
