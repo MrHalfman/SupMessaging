3f75bbf5e06ca94d9682266842aa9ac6d57583e7
adrienxp3 <158113@supinfo.com>
2014-12-04 17:38:16 +0100

    Création Class Users/Messages en JPA (ref #31)

---
+++ src/java/com/supmessaging/persistence/Messages.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.supmessaging.persistence;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author adrienxp3
+ */
+@Entity
+@Table(name = "messages")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Messages.findAll", query = "SELECT m FROM Messages m"),
+    @NamedQuery(name = "Messages.findById", query = "SELECT m FROM Messages m WHERE m.id = :id"),
+    @NamedQuery(name = "Messages.findByDate", query = "SELECT m FROM Messages m WHERE m.date = :date"),
+    @NamedQuery(name = "Messages.findByIdUserAuthor", query = "SELECT m FROM Messages m WHERE m.idUserAuthor = :idUserAuthor"),
+    @NamedQuery(name = "Messages.findByIdUserReceiver", query = "SELECT m FROM Messages m WHERE m.idUserReceiver = :idUserReceiver"),
+    @NamedQuery(name = "Messages.findByRead", query = "SELECT m FROM Messages m WHERE m.read = :read")})
+public class Messages implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "date")
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    @Basic(optional = false)
+    @NotNull
+    @Lob
+    @Size(min = 1, max = 65535)
+    @Column(name = "corpus")
+    private String corpus;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_user_author")
+    private int idUserAuthor;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_user_receiver")
+    private int idUserReceiver;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "read")
+    private int read;
+
+    public Messages() {
+    }
+
+    public Messages(Integer id) {
+        this.id = id;
+    }
+
+    public Messages(Integer id, Date date, String corpus, int idUserAuthor, int idUserReceiver, int read) {
+        this.id = id;
+        this.date = date;
+        this.corpus = corpus;
+        this.idUserAuthor = idUserAuthor;
+        this.idUserReceiver = idUserReceiver;
+        this.read = read;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCorpus() {
+        return corpus;
+    }
+
+    public void setCorpus(String corpus) {
+        this.corpus = corpus;
+    }
+
+    public int getIdUserAuthor() {
+        return idUserAuthor;
+    }
+
+    public void setIdUserAuthor(int idUserAuthor) {
+        this.idUserAuthor = idUserAuthor;
+    }
+
+    public int getIdUserReceiver() {
+        return idUserReceiver;
+    }
+
+    public void setIdUserReceiver(int idUserReceiver) {
+        this.idUserReceiver = idUserReceiver;
+    }
+
+    public int getRead() {
+        return read;
+    }
+
+    public void setRead(int read) {
+        this.read = read;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Messages)) {
+            return false;
+        }
+        Messages other = (Messages) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.supmessaging.persistence.Messages[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/java/com/supmessaging/persistence/Users.java
@@ -0,0 +1,203 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.supmessaging.persistence;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author adrienxp3
+ */
+@Entity
+@Table(name = "users")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Users.findAll", query = "SELECT u FROM Users u"),
+    @NamedQuery(name = "Users.findById", query = "SELECT u FROM Users u WHERE u.id = :id"),
+    @NamedQuery(name = "Users.findByName", query = "SELECT u FROM Users u WHERE u.name = :name"),
+    @NamedQuery(name = "Users.findByFirstname", query = "SELECT u FROM Users u WHERE u.firstname = :firstname"),
+    @NamedQuery(name = "Users.findByMail", query = "SELECT u FROM Users u WHERE u.mail = :mail"),
+    @NamedQuery(name = "Users.findByPseudo", query = "SELECT u FROM Users u WHERE u.pseudo = :pseudo"),
+    @NamedQuery(name = "Users.findByPassword", query = "SELECT u FROM Users u WHERE u.password = :password"),
+    @NamedQuery(name = "Users.findByRole", query = "SELECT u FROM Users u WHERE u.role = :role")})
+public class Users implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "name")
+    private String name;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "firstname")
+    private String firstname;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "mail")
+    private String mail;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "pseudo")
+    private String pseudo;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 255)
+    @Column(name = "password")
+    private String password;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "role")
+    private int role;
+    @JoinTable(name = "user_friendship", joinColumns = {
+        @JoinColumn(name = "id_users1", referencedColumnName = "id")}, inverseJoinColumns = {
+        @JoinColumn(name = "id_users2", referencedColumnName = "id")})
+    @ManyToMany
+    private Collection<Users> usersCollection;
+    @ManyToMany(mappedBy = "usersCollection")
+    private Collection<Users> usersCollection1;
+
+    public Users() {
+    }
+
+    public Users(Integer id) {
+        this.id = id;
+    }
+
+    public Users(Integer id, String name, String firstname, String mail, String pseudo, String password, int role) {
+        this.id = id;
+        this.name = name;
+        this.firstname = firstname;
+        this.mail = mail;
+        this.pseudo = pseudo;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    @XmlTransient
+    public Collection<Users> getUsersCollection() {
+        return usersCollection;
+    }
+
+    public void setUsersCollection(Collection<Users> usersCollection) {
+        this.usersCollection = usersCollection;
+    }
+
+    @XmlTransient
+    public Collection<Users> getUsersCollection1() {
+        return usersCollection1;
+    }
+
+    public void setUsersCollection1(Collection<Users> usersCollection1) {
+        this.usersCollection1 = usersCollection1;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Users)) {
+            return false;
+        }
+        Users other = (Users) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.supmessaging.persistence.Users[ id=" + id + " ]";
+    }
+    
+}
