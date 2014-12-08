e2cf386065a62f7d74dc4cd749b9e53769a135e0
Julien Dutriaux <jdutriaux@gmail.com>
2014-12-08 15:47:24 +0100

    Added active class management (Fixes #52)

--- src/java/com/supmessaging/tools/ActionToolBar.java
+++ src/java/com/supmessaging/tools/ActionToolBar.java
@@ -8,20 +8,39 @@ import javax.servlet.http.HttpServletRequest;
 
 public class ActionToolBar {
     public void getAdaptedToolbar(SessionCreator sessionCreator, HttpServletRequest request) {
+        // Template : <id, <isActive, link, text>>
         Map<String, List<String> > links = new HashMap<>();
 
         links.put("home", Arrays.asList("", "/SupMessaging", " <span class=\"glyphicon glyphicon-home\" aria-hidden=\"true\"></span>"));
         
         if (!sessionCreator.checkSessionExist()) {
-            // Elements apparaissant si la personne n'est pas connecté
+            // Elements apparaissants si la personne n'est pas connectée
             links.put("contact", Arrays.asList("", "/SupMessaging/contact", "Contact"));
             links.put("connect", Arrays.asList("", "/SupMessaging/connect", "Sign in"));
             links.put("registration", Arrays.asList("", "/SupMessaging/registration" ,"Sign up"));
         }
         else {
-            // Elements apparaissant si la personne est connecté 
+            // Elements apparaissants si la personne est connectée
             links.put("editProfile", Arrays.asList("", "/SupMessaging/profile", "My profile"));
             links.put("logOut", Arrays.asList("", "/SupMessaging/goodbye", "Logout"));
+        }
+        
+        switch(request.getRequestURI()) {
+            default:
+                links.get("home").set(0, "active");
+                break;
+            case "/SupMessaging/contact":
+                links.get("contact").set(0, "active");
+                break;
+            case "/SupMessaging/connect":
+                links.get("connect").set(0, "active");
+                break;
+            case "/SupMessaging/registration":
+                links.get("registration").set(0, "active");
+                break;
+            case "/SupMessaging/profile":
+                links.get("editProfile").set(0, "active");
+                break;
         }
         
         request.setAttribute("links", links);
