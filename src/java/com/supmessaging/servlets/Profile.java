34345d7506a3630c2f43503db6d25a57d8442529
Marty <martin.l.billard@gmail.com>
2014-12-09 21:26:11 +0100

    Création d'une classe de requête complexe (fixes #64)

--- src/java/com/supmessaging/servlets/Profile.java
+++ src/java/com/supmessaging/servlets/Profile.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.supmessaging.persistence.HibernateUtil;
 import com.supmessaging.persistence.Users;
 import com.supmessaging.tools.Encryption;
+import com.supmessaging.tools.ComplexRequest;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.logging.Level;
@@ -24,6 +25,7 @@ import org.hibernate.Transaction;
 
 public class Profile extends HttpServlet {
     public static final String jspView = "/WEB-INF/profile.jsp";
+    private ComplexRequest complexRequest = new ComplexRequest();
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -99,8 +101,6 @@ public class Profile extends HttpServlet {
             if(!oldPassword.isEmpty() || !newPasswordOne.isEmpty() || !newPasswordTwo.isEmpty()) {
                 checkPassword.equalizationPassword(newPasswordOne, newPasswordTwo, "newPassword");
                 try {
-                    System.out.println("Password outside function = " + oldPassword);
-                    System.out.println("Password encrypted outside function = " + getCurrentPassword (request));
                     checkPassword.validatePassword(oldPassword, "oldPassword", true);
                 } catch (NoSuchAlgorithmException ex) {
                     Logger.getLogger(Profile.class.getName()).log(Level.SEVERE, null, ex);
@@ -110,87 +110,18 @@ public class Profile extends HttpServlet {
                     request.setAttribute("errorPassword", errorsPassword);
                 }
                 else {
-                    System.out.println("I'M IN");
                     String ecryptedPassword = null;
                     try {
                         ecryptedPassword = encryption.encryptionPassword(newPasswordTwo);
                     } catch (NoSuchAlgorithmException ex) {
                         Logger.getLogger(Profile.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                    alterPassword(ecryptedPassword, request);
+                    complexRequest.alterPassword(ecryptedPassword, request);
                 }
             }
             
-            alterInformations(lastName, firstName, email, request);
+            complexRequest.alterInformations(lastName, firstName, email, request);
             this.getServletContext().getRequestDispatcher(jspView).forward(request, response);
         }
-        
-        /*
-        String oldPassword = request.getParameter("oldPassword");
-        String newPasswordOne = request.getParameter("newPasswordOne");
-        String newPasswordTwo = request.getParameter("newPasswordTwo");
-        */
-    }
-    
-    public void alterInformations(String lastName, String firstName, String email, HttpServletRequest request){
-        HttpSession session = request.getSession();
-        Session sessionHibernate = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = sessionHibernate.beginTransaction();
-
-            Query query = sessionHibernate.createQuery("update Users set "+  
-                    "name = :name, " +
-                    "firstname = :firstname, " +
-                    "mail = :mail "  +  
-                    "where pseudo = :pseudo");
-     
-            
-            query.setParameter("name", lastName );
-            query.setParameter("firstname", firstName);
-            query.setParameter("mail", email);
-            query.setParameter("pseudo", session.getAttribute("username"));
-//            query.setParameter("password", "");
-            System.out.println(query);
-            int result = query.executeUpdate();
-            tx.commit();
-            sessionHibernate.close();
-        
-    }
-    
-    public void alterPassword(String newPassword, HttpServletRequest request) {
-        
-        HttpSession session = request.getSession();
-        Session sessionHibernate = HibernateUtil.getSessionFactory().openSession();
-        Transaction tx = sessionHibernate.beginTransaction();
-
-            Query query = sessionHibernate.createQuery("update Users set "+  
-                    "password = :password " +
-                    "where pseudo = :pseudo");
-     
-            query.setParameter("pseudo", session.getAttribute("username"));
-            query.setParameter("password", newPassword);
-            System.out.println(query);
-            int result = query.executeUpdate();
-            tx.commit();
-            sessionHibernate.close();
-   
-    }
-    
-    public String getCurrentPassword (HttpServletRequest request) {
-        HttpSession session = request.getSession();
-        Session sessionHibernate = HibernateUtil.getSessionFactory().openSession();
-            Transaction tx = sessionHibernate.beginTransaction();
-
-            Query queryTest = (Query) sessionHibernate.createQuery("FROM Users u WHERE u.pseudo = :pseudo");
-            queryTest.setParameter("pseudo", session.getAttribute("username"));       
-
-            List<Users> users = queryTest.list();
-            String password = "";
-            
-            for (Users user : users){
-                password = user.getPassword();
-            }
-            tx.commit();
-            sessionHibernate.close();
-        return password;
     }
 }
---
+++ src/java/com/supmessaging/tools/ComplexRequest.java
@@ -0,0 +1,47 @@
+package com.supmessaging.tools;
+
+import com.supmessaging.persistence.HibernateUtil;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class ComplexRequest {
+    public void alterInformations(String lastName, String firstName, String email, HttpServletRequest request){
+        HttpSession session = request.getSession();
+        Session sessionHibernate = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = sessionHibernate.beginTransaction();
+
+        Query query = sessionHibernate.createQuery("update Users set "+  
+                "name = :name, " +
+                "firstname = :firstname, " +
+                "mail = :mail "  +  
+                "where pseudo = :pseudo");
+
+        query.setParameter("name", lastName );
+        query.setParameter("firstname", firstName);
+        query.setParameter("mail", email);
+        query.setParameter("pseudo", session.getAttribute("username"));
+        int result = query.executeUpdate();
+        tx.commit();
+        sessionHibernate.close();
+    }
+    
+    public void alterPassword(String newPassword, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        Session sessionHibernate = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = sessionHibernate.beginTransaction();
+
+        Query query = sessionHibernate.createQuery("update Users set "+  
+                "password = :password " +
+                "where pseudo = :pseudo");
+
+        query.setParameter("pseudo", session.getAttribute("username"));
+        query.setParameter("password", newPassword);
+        int result = query.executeUpdate();
+        tx.commit();
+        sessionHibernate.close();
+    }
+}
