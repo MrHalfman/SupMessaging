299e68ae7601ff760191fd30cbfae634a7deb5b4
Marty <martin.l.billard@gmail.com>
2014-12-12 00:32:44 +0100

    Amélioration de la sécurité de la page d'ajout (ref #20)

--- src/java/com/supmessaging/servlets/withoutjsp/AddFriend.java
+++ src/java/com/supmessaging/servlets/withoutjsp/AddFriend.java
@@ -1,6 +1,7 @@
 package com.supmessaging.servlets.withoutjsp;
 
 import com.supmessaging.tools.ComplexRequest;
+import com.supmessaging.tools.SessionCreator;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,18 +15,27 @@ public class AddFriend extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+        SessionCreator sessionCreator = new SessionCreator(request);
         ComplexRequest addFriend = new ComplexRequest();
         HttpSession session = request.getSession();
         int userId = -1;
         
-        if (request.getParameter("userId") != null) {
+        System.out.println(sessionCreator.checkSessionExist());
+        System.out.println(request.getParameter("userId") != null);
+        System.out.println(sessionCreator.checkSessionExist() && request.getParameter("userId") != null);
+        
+        
+        if(sessionCreator.checkSessionExist() && request.getParameter("userId") != null) {
             userId = Integer.parseInt(request.getParameter("userId"));
+            System.out.println(userId > -1);
+            if (userId > -1) {
+                addFriend.createRelationship((String) session.getAttribute("username"), userId);
+                System.out.println("GO LOOK THE TABLE");
+            }
+            response.sendRedirect("/SupMessaging/search");
         }
-        
-        if (userId > -1) {
-            addFriend.createRelationship((String) session.getAttribute("username"), userId);
+        else {
+            response.sendRedirect("/SupMessaging");
         }
-        
     }
 }
