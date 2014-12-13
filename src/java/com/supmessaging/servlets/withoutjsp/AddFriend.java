43d6b30dfea541040f0a4e1103e81151010ac02e
Marty <martin.l.billard@gmail.com>
2014-12-13 22:50:54 +0100

    Refactoring again!

--- src/java/com/supmessaging/servlets/withoutjsp/AddFriend.java
+++ src/java/com/supmessaging/servlets/withoutjsp/AddFriend.java
@@ -3,6 +3,7 @@ package com.supmessaging.servlets.withoutjsp;
 import com.supmessaging.tools.ComplexRequest;
 import com.supmessaging.tools.SessionCreator;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,6 +19,7 @@ public class AddFriend extends HttpServlet {
         SessionCreator sessionCreator = new SessionCreator(request);
         ComplexRequest addFriend = new ComplexRequest();
         HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username");
         int userId = -1;
         String security = null;
         
@@ -26,7 +28,7 @@ public class AddFriend extends HttpServlet {
             security = request.getParameter("security");
             
             if (userId > -1 && security.equals(session.getAttribute("security"))) {
-                addFriend.createRelationship((String) session.getAttribute("username"), userId);
+                addFriend.createRelationship(username, userId);
             }
             session.setAttribute("security", null);
             response.sendRedirect("/SupMessaging/search");
